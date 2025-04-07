1629844c4cd9a8c1112a474ab4141125c3f043d4
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2025-04-07 22:52:15 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,11 +8,9 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 GROUP_IDS = os.getenv("GROUP_IDS", "").split(",")
+ADMIN_CHAT_IDS = [int(id.strip()) for id in os.getenv("ADMIN_CHAT_IDS", "").split(",")]
 REDIRECT_BASE_URL = os.getenv("REDIRECT_BASE_URL")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
 
 app = Client("cleanup_bot", bot_token=BOT_TOKEN)
 db = MongoClient(MONGO_URI).bot_data
-
 group_ids = [int(gid.strip()) for gid in GROUP_IDS]
-redirect_url = REDIRECT_BASE_URL
