70b00c938a0f63c74ec7c1f8fcbdb7a74bdc2565
aklinksz002 <aklinkszbot002@gmail.com>
2025-04-08 10:02:10 +0530

    Update and rename config.py to bot/config.py

--- bot/config.py
+++ bot/config.py
@@ -10,7 +10,7 @@ class Config:
     MONGODB_URI = os.getenv("MONGODB_URI", "")
     OWNER_ID = int(os.getenv("OWNER_ID", "0"))  # Your Telegram user ID
     GROUP_IDS = os.getenv("GROUP_IDS", "").split(",")  # Comma-separated list of group IDs
-    REDIRECT_BASE = os.getenv("REDIRECT_BASE", "https://aklinksz1206.blogspot.com/2025/04/aklinksz-invite-bot.html?")
+    REDIRECT_BASE = os.getenv("REDIRECT_BASE")
     TIMEZONE = os.getenv("TIMEZONE", "Asia/Kolkata")
 
 config = Config()
