0b8cad3bf6094d566af76b9ce0738bfe6e5b68ac
aklinksz002 <aklinkszbot002@gmail.com>
2025-04-08 12:29:09 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,8 +2,10 @@ from pyrogram import Client
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from config import config
 from scheduler.cleanup_jobs import schedule_cleanup_jobs
-from handlers import admin_panel, broadcast, welcome_handler, rejoin_request, join_leave_cleaner
+from handlers import admin_panel, broadcast, welcome_handler, rejoin_request
 from helpers.db import init_db
+from webserver import run_webserver
+import asyncio
 
 app = Client(
     name="cleanup_bot",
@@ -20,7 +22,6 @@ def register_handlers():
     broadcast.register(app)
     welcome_handler.register(app)
     rejoin_request.register(app)
-    join_leave_cleaner.register(app)
 
 # Run bot and scheduler
 async def run():
@@ -31,12 +32,10 @@ async def run():
     print("Bot started.")
 
 if __name__ == "__main__":
-    import asyncio
-    from webserver import run_webserver
-
     loop = asyncio.get_event_loop()
 
     # Start bot and web server in parallel
+    loop.create_task(app.start())
     loop.create_task(run())
     loop.run_in_executor(None, run_webserver)
-    app.run()
+    loop.run_forever()
