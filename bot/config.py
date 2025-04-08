9a90058aee156ed1066c25ac9430891cd08e7a9a
aklinksz002 <aklinkszbot002@gmail.com>
2025-04-08 12:39:57 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ class Config:
     API_ID = int(os.getenv("API_ID", "123456"))
     API_HASH = os.getenv("API_HASH", "")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    MONGODB_URI = os.getenv("MONGODB_URI", "")
+    MONGODB_URI = os.getenv("MONGO_URI", "")
     OWNER_ID = int(os.getenv("OWNER_ID", "0"))  # Your Telegram user ID
     GROUP_IDS = os.getenv("GROUP_IDS", "").split(",")  # Comma-separated list of group IDs
     REDIRECT_BASE = os.getenv("REDIRECT_BASE")
