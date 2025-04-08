7ff29bebef9dd6774457ebe3159b29585e463ad6
aklinksz002 <aklinkszbot002@gmail.com>
2025-04-08 09:56:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 class Config:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    MONGO_URI = os.getenv("MONGO_URI")
-    GROUP_IDS = list(map(int, os.getenv("GROUP_IDS", "").split(",")))
-    ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
-    REDIRECT_BASE_URL = os.getenv("REDIRECT_BASE_URL")
+    API_ID = int(os.getenv("API_ID", "123456"))
+    API_HASH = os.getenv("API_HASH", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    MONGODB_URI = os.getenv("MONGODB_URI", "")
+    OWNER_ID = int(os.getenv("OWNER_ID", "0"))  # Your Telegram user ID
+    GROUP_IDS = os.getenv("GROUP_IDS", "").split(",")  # Comma-separated list of group IDs
+    REDIRECT_BASE = os.getenv("REDIRECT_BASE", "https://aklinksz1206.blogspot.com/2025/04/aklinksz-invite-bot.html?")
+    TIMEZONE = os.getenv("TIMEZONE", "Asia/Kolkata")
 
 config = Config()
