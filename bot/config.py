2401dc5703d57d630beb7bd7ffff8485a40c5f9f
aklinksz002 <aklinkszbot002@gmail.com>
2025-04-08 13:01:35 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.getenv("API_ID", "123456"))
+    API_ID = int(os.getenv("API_ID", ""))
     API_HASH = os.getenv("API_HASH", "")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "")
     MONGODB_URI = os.getenv("MONGODB_URI", "")
